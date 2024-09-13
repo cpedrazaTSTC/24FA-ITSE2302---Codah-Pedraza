2c7182889667fc035aec79a95329fd414197ba99
Codah Pedraza <cpedraza138939@mymail.tstc.edu>
2024-09-12 23:16:23 -0500

    Added files for Mastery 1

---
+++ JavaScript/logic.js
@@ -0,0 +1,28 @@
+/* Codah Pedraza
+   9/4/24
+   24/FA ITSE 2302-7PDH2
+   4A Intro To C+ Programming App
+*/
+
+// Assigns the variable "fname" to my first name 
+var fname = "Codah";
+// Assigns the variable "lname" to my last name
+var lname = "Pedraza";
+// Assigns the variable "stringname" to my custom string of choice
+var stringname = "Welcome to my website!";
+// Allows a certain value to be considered true/false (used in booleans)
+let x = false
+// Assigns the variable "theTruth" to a boolean
+var theTruth = Boolean(x);
+// Assigns the variable "value1" to the number "10"
+var value1 = 10;
+// Assigns the variable "value2" to the number "15"
+var value2 = 15;
+// Assigns the variable "value3" to the number "25"
+var value3 = 25;
+
+// Prints out to the console
+console.log("My name is " + fname + " " + lname);
+console.log(stringname.length);
+console.log("The sum of " + value1 + " and " + value2 + " is equal to " + value3);
+console.log("The variable theTruth is storing the value " + Boolean(theTruth) + ". " + "The data type of theTruth is " + typeof false);
